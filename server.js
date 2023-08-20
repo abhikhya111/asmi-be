a0934b2251e389eea07a5399b67281603cf48489
Abhikhya <abhikhya.ashi@gmail.com>
2023-08-20 14:03:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -39,8 +39,8 @@ const URL = `mongodb+srv://abhikhya:ashi3666@crud.yjywtf4.mongodb.net/dummyDatab
         console.log('Error: ', error.message);
     }
 
-const productRouter = require("../backend/app/routes/product.route");
-const userRouter = require("../backend/app/routes/user.routes");
+const productRouter = require("../asmi-be/app/routes/product.route");
+const userRouter = require("../asmi-be/app/routes/user.routes");
 
 app.use("/api/products", productRouter);
 app.use("/api/users/", userRouter )
